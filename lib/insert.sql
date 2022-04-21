22ceef37e1e7ff8fedf1637f31df008cb1cb5459
Candice Smith MacKenzie <Mahony8@aol.com>
2022-04-20 22:13:52 -0400

    commit tree aa62a61aa11283e946498a3f95f9c2f172141c5a

---
+++ lib/insert.sql
@@ -0,0 +1,11 @@
+INSERT INTO bears VALUES(1, "Mr. Chocolate", 20, "M", "brown", "friendly", 1);
+INSERT INTO bears VALUES (2, "Rowdy", 10, "M", "brown", "unfriendly", 1);
+INSERT INTO bears VALUES (3, "Tabitha", 6, "F", "black", "calm", 1);
+INSERT INTO bears VALUES (4, "Sergeant Brown", 1, "M", "reddish brown", "friendly", 0);
+INSERT INTO bears VALUES (5, "Melissa", 13, "F", "black", "aggressive", 1);
+INSERT INTO bears VALUES (6, "Grinch", 2, "M", "brown", "sweet", 1);
+INSERT INTO bears VALUES (7, "Wendy", 6, "F", "brown", "unfriendly", 1);
+INSERT INTO bears VALUES (8, NULL, 2, "M", "brown", "aggressive", 0);
+
+
+
